65a1a8be56c66afc36ac7a9205745cf3c4f07060
Ahri <ahripyx@gmail.com>
2025-11-28 00:35:18 -0500

    Created 3 second loading screen delay

--- app/src/main/java/com/example/track_a_cal/LoadingActivity.java
+++ app/src/main/java/com/example/track_a_cal/LoadingActivity.java
@@ -1,4 +1,24 @@
 package com.example.track_a_cal;
 
-public class LoadingActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+
+        // Adding 3 second delay
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+
+    }
 }
